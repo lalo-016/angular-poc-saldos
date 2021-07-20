1483e158af7eff31a396d6ae10c1258963b81950
Eduardo <eduardo.velascos@agilethought.com>
2021-07-20 00:17:43 -0500

    update url

--- src/app/services/consulta.service.ts
+++ src/app/services/consulta.service.ts
@@ -10,6 +10,6 @@ export class ConsultaService {
   constructor(private http: HttpClient) { }
 
   getItems(){
-    return this.http.get('http://ec2-3-81-147-145.compute-1.amazonaws.com:7777/portalserver/services/rest/api/v1/private/keep/alive');
+    return this.http.get('https://rickandmortyapi.com/api/episode');
   }
 }
